8a87a5cd5ea4eeab508d12e48dcac0ed14f93cf6
qorbaxk <bekgyber3@hanmail.net>
2022-08-12 12:03:33 +0900

    게임 플레이 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,71 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-
   //랜덤 번호 지정
-  let [answerNum,setAnswerNum] = useState<number>(0);
+  let [answerNum, setAnswerNum] = useState<number>(0);
   //기회 지정
-  let chances:number = 5;
+  let [chances, setChances]=useState<number>(5);
   //유저가 적은 번호
-  let [userInput,setUserInput] = useState<number | any>();
+  let [userInput, setUserInput] = useState<number | any>();
   //게임오버
-  let gameOver:boolean = false;
+  let [gameOver, setGameOver]=useState<boolean>(false);
   //히스토리 배열
-  let history:number[] = [];
+  let [history, setHistory]=useState<number[]|any[]>([]);
+  //멘트부분
+  let [announcement, setAnnouncement] =
+    useState<string>("내가 생각하는 숫자를 맞춰보게");
 
   //랜덤숫자 뽑는 함수
-  useEffect(()=>{
-    setAnswerNum(Math.floor(Math.random()*100)+1);
-  },[]);
-    
-  console.log("정답",answerNum);
-  console.log("내가 넣은 숫자",userInput);
+  useEffect(() => {
+    setAnswerNum(Math.floor(Math.random() * 100) + 1);
+  }, []);
 
-  
+  console.log("정답", answerNum);
+
+  //게임 함수
+  const gameStart = () => {
+    //유저가 범위 밖의 번호를 쓰면 알려줌 기회는 그대로
+    if (userInput < 1 || userInput > 100){
+      return setAnnouncement("1과 100사이의 값을 입력하게");
+    }
+    //유저가 이미 입력한 숫자를 또 입력하면 알려줌 기회는 그대로
+    if (history.includes(userInput)){
+      return setAnnouncement("이미 입력한 값이네, 다른 값을 입력하게");
+    }
+
+    //플레이 할때마다 기회 1씩 줄어
+    chances--;
+    setChances(chances);
+
+    //유저가 랜덤번호를 맞추는 과정
+    if(userInput<answerNum){
+      setAnnouncement("틀렸네, 더 높은 값을 입력하게");
+    }else if(userInput>answerNum){
+      setAnnouncement("틀렸네, 더 낮은 값을 입력하게");
+    }else if(userInput==answerNum){
+      setAnnouncement("정답이네!!");
+      setGameOver(true);
+    }
+
+    //유저가 입력한 값을 배열에 저장
+    history.push(userInput);
+    setHistory(history);
+   
+
+    //기회가 0이 되면 게임오버
+    if(chances<1 && userInput!==answerNum){
+      setAnnouncement(`끝났네! 정답은 ${answerNum}일세`);
+      setGameOver(true);
+    }
+  };
+
+
+  //리셋함수
+  const reset = () =>{
+    
+  }
+
 
 
 
@@ -33,21 +76,24 @@ function App() {
 
       <div className="main">
         <div className="ment">
-          <p>내가 생각하는 숫자를 맞춰보게</p>
-          <p>기회는 총 다섯번 일세</p>
+          <p>{announcement}</p>
+          {gameOver?null:<p>기회는 총 {chances}번 일세</p>}
           <div>
             <input
               className="user_input"
               type="number"
               placeholder="1부터 100중에 숫자 고르기!"
-              onChange={(e)=>setUserInput(e.target.value)}
+              onChange={(e) => setUserInput(e.target.value)}
             />
           </div>
           <div>
-            <button className="btn">다음▶</button>
+            <button className={gameOver?"unactivebtn":
+          "activebtn"} onClick={gameStart} disabled={gameOver}>
+              다음▶
+            </button>
           </div>
           <div>
-            <button className="btn2">리셋◀</button>
+            <button className="btn2" onClick={reset}>리셋◀</button>
           </div>
         </div>
       </div>
