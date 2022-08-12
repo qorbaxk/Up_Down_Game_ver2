86f8db48f247fcb62c6463a19487814a417b1fe6
qorbaxk <bekgyber3@hanmail.net>
2022-08-12 12:52:59 +0900

    이미지 구상중

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,15 @@ function App() {
   //랜덤 번호 지정
   let [answerNum, setAnswerNum] = useState<number>(0);
   //기회 지정
-  let [chances, setChances]=useState<number>(5);
+  let [chances, setChances] = useState<number>(5);
   //유저가 적은 번호
   let [userInput, setUserInput] = useState<number | any>();
+  //인풋 텍스트 값
+  let [text, setText] = useState<string>("");
   //게임오버
-  let [gameOver, setGameOver]=useState<boolean>(false);
+  let [gameOver, setGameOver] = useState<boolean>(false);
   //히스토리 배열
-  let [history, setHistory]=useState<number[]|any[]>([]);
+  let [history, setHistory] = useState<number[] | any[]>([]);
   //멘트부분
   let [announcement, setAnnouncement] =
     useState<string>("내가 생각하는 숫자를 맞춰보게");
@@ -21,16 +23,16 @@ function App() {
     setAnswerNum(Math.floor(Math.random() * 100) + 1);
   }, []);
 
-  console.log("정답", answerNum);
-
   //게임 함수
   const gameStart = () => {
     //유저가 범위 밖의 번호를 쓰면 알려줌 기회는 그대로
-    if (userInput < 1 || userInput > 100){
+    if (userInput < 1 || userInput > 100) {
+      setText("");
       return setAnnouncement("1과 100사이의 값을 입력하게");
     }
     //유저가 이미 입력한 숫자를 또 입력하면 알려줌 기회는 그대로
-    if (history.includes(userInput)){
+    if (history.includes(userInput)) {
+      setText("");
       return setAnnouncement("이미 입력한 값이네, 다른 값을 입력하게");
     }
 
@@ -39,36 +41,32 @@ function App() {
     setChances(chances);
 
     //유저가 랜덤번호를 맞추는 과정
-    if(userInput<answerNum){
+    if (userInput < answerNum) {
       setAnnouncement("틀렸네, 더 높은 값을 입력하게");
-    }else if(userInput>answerNum){
+    } else if (userInput > answerNum) {
       setAnnouncement("틀렸네, 더 낮은 값을 입력하게");
-    }else if(userInput==answerNum){
+    } else if (userInput == answerNum) {
       setAnnouncement("정답이네!!");
       setGameOver(true);
     }
-
     //유저가 입력한 값을 배열에 저장
     history.push(userInput);
     setHistory(history);
-   
 
-    //기회가 0이 되면 게임오버
-    if(chances<1 && userInput!==answerNum){
+    //인풋값 초기화
+    setText("");
+
+    //게임오버
+    if (chances < 1 && userInput != answerNum) {
       setAnnouncement(`끝났네! 정답은 ${answerNum}일세`);
       setGameOver(true);
     }
   };
 
-
   //리셋함수
-  const reset = () =>{
-    
-  }
-
-
-
-
+  const reset = () => {
+    window.location.reload();
+  };
 
   return (
     <div className="background">
@@ -77,23 +75,38 @@ function App() {
       <div className="main">
         <div className="ment">
           <p>{announcement}</p>
-          {gameOver?null:<p>기회는 총 {chances}번 일세</p>}
+          {gameOver ? null : <p>기회는 총 {chances}번 일세</p>}
           <div>
-            <input
-              className="user_input"
-              type="number"
-              placeholder="1부터 100중에 숫자 고르기!"
-              onChange={(e) => setUserInput(e.target.value)}
-            />
+            <div>
+              <img src="https://mblogthumb-phinf.pstatic.net/MjAyMDA2MjhfMjQy/MDAxNTkzMzA5NjMwNTE0.gmsZfbWPdTmbkN_i6z2Ci91t-vwBnmqVtI_SKJeujv8g.5UqgTTALREXR--eOPGyIqRhcJD11QCFj74f-6jrL470g.GIF.dlswns020/attack1.gif?type=w2" />
+            </div>
+
+            {gameOver ? null : (
+              <input
+                className="user_input"
+                type="number"
+                placeholder="1부터 100중에 숫자 고르기!"
+                value={text}
+                onChange={(e) => {
+                  setUserInput(e.target.value);
+                  setText(e.target.value);
+                }}
+              />
+            )}
           </div>
           <div>
-            <button className={gameOver?"unactivebtn":
-          "activebtn"} onClick={gameStart} disabled={gameOver}>
-              다음▶
+            <button
+              className={gameOver ? "unactivebtn" : "activebtn"}
+              onClick={gameStart}
+              disabled={gameOver}
+            >
+              이거 맞지?
             </button>
           </div>
           <div>
-            <button className="btn2" onClick={reset}>리셋◀</button>
+            <button className="btn2" onClick={reset}>
+              다시 할래!
+            </button>
           </div>
         </div>
       </div>
